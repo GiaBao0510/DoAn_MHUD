867393cd72b8da33c46515fc7d0c22646b025967
GiaBao0510 <pgiabao2002@gmail.com>
2023-10-08 13:58:41 +0700

    Update  KNN .By GiaBao

--- nhom22_statlog.py
+++ nhom22_statlog.py
@@ -1,6 +1,8 @@
+import random
 import numpy as np
 import pandas as pd
 from sklearn.metrics import accuracy_score #Độ chính xác
+from sklearn.metrics import f1_score #F1
 
 #Đọc dữ liệu Train - Test
 data_train = pd.read_csv("data_statlog/sat.trn",encoding='utf-8',header=None,sep=' ')
@@ -17,16 +19,34 @@ y_test = data_test.values[:,-1]        #Lấy tất cả các hàng của cột 
 '''     ---  1.KNN  ---   '''
 from sklearn.neighbors import KNeighborsClassifier
 
-#0. Lặp Tầm 10 lần
-lap = 10
+#0. Các biến tham gia
+lap = 5        #Cho biết là sẽ lặp 5 lần
+kqLap = []      #Mảng này dùng để gom kết quả
+tongKQ = float(0)
 
-#1.Khởi tạo mô hình knn với 5 phần tử liền kề
-model_KNN = KNeighborsClassifier(n_neighbors=100)
+#Vòng lặp
+for i in range(lap):
+    print("Lặp lần thứ: ",i+1)
+    #1.Khởi tạo mô hình knn với sự random 70 - 140  phần tử liền kề
+    model_KNN = KNeighborsClassifier(n_neighbors=random.randint(70,140))
 
-#2. Huấn luyện mô hình
-model_KNN.fit(x_train,y_train)
+    #2. Huấn luyện mô hình
+    model_KNN.fit(x_train,y_train)
 
-#3. Dự đoán nhãn cho mô hình
-y_pred = model_KNN.predict(x_test)
+    #3. Dự đoán nhãn cho mô hình
+    y_pred = model_KNN.predict(x_test)
 
-print('Nhãn dự đoán: ',y_pred)
+    #4. Đánh giá 
+    f1 = f1_score(y_test,y_pred,average="macro")
+    print('Độ chính xác: ',accuracy_score(y_test,y_pred)*100)
+    print(f'F1-score: {f1*100:.2f}%')
+
+    #5.Lưu vào mảng
+    kqLap.append(f1)
+
+#Vòng lặp tổng kết quả
+for i in range(lap):
+    tongKQ += kqLap[i]
+
+#Trung bình cộng của F1 khi 5 lần lặp
+print(f"Trung Bình cộng của F1 khi thực hiện 5 lần lặp: {(tongKQ/lap)*100:.2f}%")
